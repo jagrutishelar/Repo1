0c432bce8f5d7689074e93a043a7f66369566deb
Admin <Admin@DESKTOP-JJ0JB3F>
2021-03-17 22:04:11 +0530

    dfd

--- 2CSA2/src/testng/P5.java
+++ 2CSA2/src/testng/P5.java
@@ -1,26 +1,26 @@
 package testng;
 import org.testng.annotations.Test;
 
-public class P4 {
+public class P5 {
 	
-	@Test
+	@Test(invocationCount=3)
 	public void pen() {
 		System.out.println("from pen");
 	}
 	
-	@Test(priority=2)
+	@Test(invocationCount=4)
 	public void mouse() {
 		System.out.println("from mouse");
 	}
 	
-	@Test(priority=1)
-	public void keyboard() {
-		System.out.println("from keyboard");
-	}
-	
-	@Test
+	@Test(invocationCount=6)
 	public void duster() {
 		System.out.println("from duster");
+	}
+	
+	@Test(invocationCount=5)
+	public void keyboard() {
+		System.out.println("from keyboard");
 	}
 
 }
--- 2CSA2/src/testng/P6.java
+++ 2CSA2/src/testng/P6.java
@@ -1,26 +1,26 @@
 package testng;
 import org.testng.annotations.Test;
 
-public class P4 {
+public class P6 {
 	
-	@Test
+	@Test(priority=5,invocationCount=3)
 	public void pen() {
 		System.out.println("from pen");
 	}
 	
-	@Test(priority=2)
+	@Test(priority=10,invocationCount=4)
 	public void mouse() {
 		System.out.println("from mouse");
 	}
 	
-	@Test(priority=1)
-	public void keyboard() {
-		System.out.println("from keyboard");
-	}
-	
-	@Test
+	@Test(priority=2,invocationCount=6)
 	public void duster() {
 		System.out.println("from duster");
+	}
+	
+	@Test(priority=1,invocationCount=5)
+	public void keyboard() {
+		System.out.println("from keyboard");
 	}
 
 }
