151ddec6b0b37f7b45f1b74ea993268dce4c598d
Admin <Admin@DESKTOP-JJ0JB3F>
2021-03-22 20:05:13 +0530

    fgdfgfh

---
+++ 2CSA2/src/qsp/P7.java
@@ -0,0 +1,25 @@
+package qsp;
+
+
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P7 {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.naukri.com");
+		Set<String> allWindowHandles = driver.getWindowHandles();
+		for(String wh :allWindowHandles) {
+			System.out.println(wh);
+		}
+		System.out.println("No of addresses: "+allWindowHandles.size());
+		driver.quit();
+	}
+
+}
